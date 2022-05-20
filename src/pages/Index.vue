d528451725aec5bd2cdbce6cdc44b8b14d29b9be
Jose Seie <joseeugenioseie@hotmail.com>
2022-05-20 20:45:22 +0200

    ui: tela de user

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,54 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+
+    <div class="text-center q-gutter-lg q-pa-lg">
+
+      <!--Espaço da foto-->
+      <div class="">
+
+        <img
+            src="https://avatars.githubusercontent.com/u/18400142?v=4"
+            alt="José Seie"
+            style="border-radius: 100%;width: 100px;height: 100px"
+        >
+        
+      </div>
+
+      <!--Espaço com o texto-->
+      <div class="">
+
+        <div class="text-h6" style="font-size: 20pt">
+          Oi, José
+        </div>
+        <div class="text-grey-5">
+          Nossa calculadora online oferece descontos com base no numero de cursos da Explicador que frequentou
+        </div>
+      </div>
+
+      <!--Espaço do botão-->
+      <div class="q-pa-lg">
+
+        <q-btn
+          label="Calcular"
+          unelevated
+          color="blue-8"
+          class="full-width"
+        />
+
+      </div>
+
+      <!--Força que os elementos subam-->
+
+      <div style="height: 9vh;"></div>
+
+    </div>
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
 
-export default defineComponent({
+export default {
   name: 'PageIndex'
-})
+}
 </script>
